93ed14a15e433bbe60a928be2bae73a306e5a8d5
Daniel Luiz <156124027+danielluiz07@users.noreply.github.com>
2024-07-26 09:40:13 +0000

    Jogo Java

--- lab04/Jogo.java
+++ lab04/Jogo.java
@@ -3,36 +3,26 @@ package lab04;
 import java.util.Scanner;
 
 public class Jogo {
-    // java lab04/Repeticoes1.java
     public static void main(String[] args) {
-        /**
-         * while: Executa um bloco de código enquanto uma condição específica for
-         * verdadeira.
-         */
 
         var sc = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        var numero = sc.nextInt();
-        var sorteado = 26;
 
-        while (numero != sorteado) {
-            System.out.println("Digite um número: ");
-            var numero2 = sc.nextInt();
-            if(numero == sorteado){
-                System.out.println("Parabens vc acertou!");
-                break;
-            }else if(numero2 != sorteado){
-                System.out.println("Digite um número: ");
-            }else if (numero2 == sorteado) {
-                System.out.println("Numero Correto");
-                
-            }else if(numero2 < sorteado){
-                System.out.println("Digite um número maior");
-                
-            } else if (numero2 > sorteado) {
-                System.out.println("Digite um número menor");
+        int primeiro = (int) (Math.random() * 100) + 1;
+        int numero;
+
+        System.out.println("Digite um número entre 1 e 100: ");
+        numero = sc.nextInt();
+
+        while (numero != primeiro) {
+            if (numero > primeiro) {
+                System.out.println("Digite um número menor!");
+            } else {
+                System.out.println("Digite um número maior!");
             }
+
+            System.out.println("Digite um número: ");
+            numero = sc.nextInt();
         }
+        System.out.println("Parabéns! Você acertou o número.");
     }
-    
 }
